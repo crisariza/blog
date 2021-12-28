2556752e121c35f323643df44875c9587f6c5ef2
Cristian Ariza <mecrisariza@gmail.com>
2021-12-28 14:07:13 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>Sir Danny Blog</title>
+        <title>Cristian Ariza Blog</title>
       </Head>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
