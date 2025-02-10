494b97064e6d16ca8e1164105bfb5aac9dc83d50
crisariza <mecrisariza@gmail.com>
2025-02-10 01:39:59 -0300

    phone redirect

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/phone',
+                destination: 'https://api.whatsapp.com/send?phone=541123885841',
+                permanent: true,
+            },
+        ];
+    },
+};
