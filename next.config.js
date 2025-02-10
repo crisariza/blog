a1b34c2ce8d66632cf543ea8ccdfaf9d83101677
crisariza <mecrisariza@gmail.com>
2025-02-10 01:41:00 -0300

    girlfriend

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ module.exports = {
                 destination: 'https://api.whatsapp.com/send?phone=541123885841',
                 permanent: true,
             },
+            {
+                source: '/girlfriend',
+                destination: 'https://www.instagram.com/agust6na/',
+                permanent: true,
+            },
         ];
     },
 };
+
