f632ee43a45d18799b1f3c48d055092f28961e89
crisariza <mecrisariza@gmail.com>
2025-07-08 13:58:11 -0300

    latest

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const viewport = {
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+    process.env.NEXT_PUBLIC_SITE_URL || "http://crisariza.com"
   ),
   title: "Cristian Ariza - Software Engineer",
   description:
